36015dd7f9ecf4557b4db177c0623ea5df3ca909
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2024-02-01 13:00:31 -0500

    added client, seed, and seedData to server folder

---
+++ server/db/client.js
@@ -0,0 +1,10 @@
+const { Client } = require('pg');
+
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:54321/revive';
+
+const client = new Client({
+  connectionString,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
+
+module.exports = client;
---
+++ server/db/seedData.js
@@ -0,0 +1,15 @@
+const exercises = [
+    {
+        name: 'jumping jacks',
+        description: 'jump in jack',
+        difficulty: 'beginner',
+        imgUrl: 'https://www.spotebi.com/wp-content/uploads/2014/10/jumping-jacks-exercise-illustration.jpg'
+    }
+]
+
+const users = [
+    {
+        
+
+    }
+]
