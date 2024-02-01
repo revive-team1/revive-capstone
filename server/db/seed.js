8e079ea1ce55df9a42e7f5fa46ab492b0a1cb53e
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2024-02-01 16:38:34 -0500

    created CreateTables function in seed.js

--- server/db/seed.js
+++ server/db/seed.js
@@ -18,3 +18,61 @@ async function dropTables() {
         throw error;
     }
 }
+
+async function createTables() {
+    try {
+        console.log('Building All Tables...');
+        await client.query(`
+        CREATE TABLE exercises (
+            exercise_id SERIAL PRIMARY KEY,
+            name TEXT,
+            description TEXT,
+            "imgUrl" TEXT,
+            difficulty TEXT
+        );
+        CREATE TABLE users (
+            users_id SERIAL PRIMARY KEY,
+            firstname TEXT,
+            lastname TEXT,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL, 
+            email TEXT UNIQUE NOT NULL
+        );
+        CREATE TABLE recipes (
+            recipe_id SERIAL PRIMARY KEY, 
+            name TEXT, 
+            difficulty TEXT, 
+            recipe_yield TEXT, 
+            "imgUrl" TEXT,
+            description TEXT
+        );
+        CREATE TABLE selfCare (
+            selfCare_id SERIAL PRIMARY KEY,
+            name TEXT, 
+            description TEXT, 
+            article_url TEXT
+        );
+        CREATE TABLE calendars (
+            calendar_id SERIAL PRIMARY KEY,
+            user_id,
+            activity_name TEXT,
+            activity_description TEXT,
+            activity_date DATE,
+            activity_time TIME
+        );
+        CREATE TABLE favorite_recipes (
+            favorite_id SERIAL PRIMARY KEY,
+            recipe_id INTEGER REFERENCES recipes(recipe_id),
+            user_id INTEGER REFERENCES users(user_id)
+        );
+        CREATE TABLE favorite_exercises (
+            favorite_id SERIAL PRIMARY KEY,
+            exercise_id INTEGER REFERENCES exercises(exercise_id),
+            user_id INTEGER REFERENCES users(user_id)
+        );
+        `);  
+    } catch (error) {
+        console.log('error createing tables')
+        throw error;
+    }
+}
