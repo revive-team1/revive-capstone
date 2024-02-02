3bdfece70a09d8f0ccb64d0a48cea3f5a276a113
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2024-02-02 16:25:14 -0500

    created api routes for exercises and users

---
+++ server/db/api/exercises.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+
+const { getAllExercises,
+    getExercisesById,
+    createExercises,
+    updateExercises, 
+    deleteExercises } = require('../sqlHelperFunctions/exercises');
+const { authRequired } = require('./utils');
+
+//GET - /api/exercises - get all exercises
+router.get('/', async (req, res, next) => {
+    try {
+        const exercises = await getAllExercises();
+        res.send(exercises);
+    } catch(error) {
+        next(error);
+    }
+});
+
+//GET - /api/exercises/:id - get a single exercise by id
+router.get('/:id', async (req, res, next) => {
+    try {
+        const exercise = await getExercisesById(req.params.id);
+        res.send(exercise);
+    } catch (error) {
+        next(error);
+    }
+});
+
+//POST - /api/exercises - create a new exercise
+router.post('/', authRequired, async (req, res, next) => {
+    try {
+        const exercise = await createExercises(req.body);
+        res.send(exercise);
+    } catch (error) {
+        next(error);
+    }
+});
+
+//PUT - /api/exercises/:id - update a single exercise by id
+router.put('/:id', authRequired, async (req, res, next) => {
+    try {
+        const exercise = await updateExercises(req.params.id, req.body);
+        res.send(exercise);
+    } catch (error) {
+        next(error);
+    }
+});
+
+//DELETE - /api/exercises/:id - delete a single exercise by id
+router.delete('/:id', authRequired, async (req, res, next) => {
+    try {
+        const exercise = await deleteExercises(req.params.id);
+        res.send(exercise);
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = router;
---
+++ server/db/api/users.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../..secrets");
+const SALT_ROUNDS = 10;
+
+const { 
+    createUser, 
+    getAllUsers,
+    getUserByEmail
+} = require('../sqlHelperFunctions/users');
+
+// GET - /api/users - get all users
+router.get('/', async(req, res, next) => {
+    try {
+        const users = await getAllUsers();
+        res.send(users);
+    } catch (error) {
+        console.log('error from api router', error);
+        next(error);
+    }
+});
+
+// POST - /api/users/register - create a new user
+router.post('/register', async (req, res, next) => {
+    try {
+        const { firstname, lastname, username, password, email } = req.body;
+        const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+        const user = await createUser({ firstname, lastname, username, password, email: hashedPassword });
+        delete user.password;
+        
+        const token = jwt.sign(user, JWT_SECRET);
+
+        res.cookie('token', token, {
+            sameSite: 'strict',
+            httpOnly: true,
+            signed: true,
+        });
+        
+        delete user.password;
+
+        res.send({ token, user });
+    } catch (error) {
+        console.log('error from POST api router', error);
+        next(error);
+    }
+});
+
+router.post('/login', async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const users = await getUserByEmail(email);
+
+        if (users.length === 1) {
+            const user = users[0];
+            const validPassword = await bcrypt.compare(password, user.password);
+
+            if (validPassword) {
+                const token = jwt.sign(user, JWT_SECRET);
+
+                res.cookie('token', token, {
+                    sameSite: 'strict',
+                    httpOnly: true,
+                    signed: true
+                });
+
+                delete user.password;
+
+                res.send({ token, user });
+            } else {
+                res.status(401).send({ error: 'Invalid password' });
+            }
+        } else {
+            res.status(401).send({ error: 'User not found' });
+        }
+    } catch (error) {
+        console.log('error from post api router with login', error);
+        next(error);
+    }
+});
+
+router.post('/logout', async (req, res, next) => {
+    try {
+        res.clearCookie('token', {
+            sameSite: 'strict',
+            httpOnly: true,
+            signed: true,
+        });
+        res.send({
+            loggedIn: false,
+            message: 'Logged Out',
+        });
+    } catch (error) {
+        console.log('error from post api router with logout', error);
+        next(error);
+    }
+});
+
+module.exports = router;
+
