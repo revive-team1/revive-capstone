17833b06979f1a57fe81dd49dffb6dc7e511c5ae
Melissa J. Chin <melissa1jchin@gmail.com>
2024-02-02 14:37:23 -0600

    adding api folder and api routing files

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,12 +1,19 @@
-const client = require('./client');
-const { exercises, users, selfCare, recipes, calendars, favoriteRecipes, favoriteExercises } = require('./seedData')
-
+const client = require("./client");
+const {
+  exercises,
+  users,
+  selfCare,
+  recipes,
+  calendars,
+  favoriteRecipes,
+  favoriteExercises,
+} = require("./seedData");
 
 // drop tables for clients, membership, exercises
 async function dropTables() {
-    try {
-        console.log("Dropping All Tables...");
-        await client.query(`
+  try {
+    console.log("Dropping All Tables...");
+    await client.query(`
       DROP TABLE IF EXISTS exercises CASCADE;
       DROP TABLE IF EXISTS users CASCADE;
       DROP TABLE IF EXISTS recipes CASCADE;
@@ -15,15 +22,15 @@ async function dropTables() {
       DROP TABLE IF EXISTS calendars CASCADE;
       DROP TABLE IF EXISTS selfCare CASCADE;
     `);
-    } catch (error) {
-        throw error;
-    }
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function createTables() {
-    try {
-        console.log('Building All Tables...');
-        await client.query(`
+  try {
+    console.log("Building All Tables...");
+    await client.query(`
         CREATE TABLE exercises (
             exercise_id SERIAL PRIMARY KEY,
             name TEXT,
@@ -64,167 +71,187 @@ async function createTables() {
         );
         CREATE TABLE favoriteRecipes (
             favorite_id SERIAL PRIMARY KEY,
-            recipe_id INTEGER REFERENCES recipes(recipe_id),
-            user_id INTEGER REFERENCES users(user_id)
+            user_id INTEGER REFERENCES users(user_id),
+            recipe_id INTEGER REFERENCES recipes(recipe_id)
         );
         CREATE TABLE favoriteExercises (
             favorite_id SERIAL PRIMARY KEY,
-            exercise_id INTEGER REFERENCES exercises(exercise_id),
-            user_id INTEGER REFERENCES users(user_id)
+            user_id INTEGER REFERENCES users(user_id),
+            exercise_id INTEGER REFERENCES exercises(exercise_id)
+            
         );
         `);
-    } catch (error) {
-        console.log('error creating tables')
-        throw error;
-    }  
+  } catch (error) {
+    console.log("error creating tables");
+    throw error;
+  }
 }
 
 const createInitialCalendars = async () => {
-    try {
-        for (const date of calendars) {
-            await client.query(
-                `
+  try {
+    for (const appointment of calendars) {
+      await client.query(
+        `
             INSERT INTO calendars(user_id, activity_date, activity_name, activity_time, activity_description, activity_link)
             VALUES($1, $2, $3, $4, $5, $6);
         `,
-                [
-                    date.user_id,
-                    date.activity_date,
-                    date.activity_name,
-                    date.activity_time,
-                    date.activity_description,
-                    date.activity_link,
-                ]
-            );
-        }
-        console.log("created calendars!");
-    } catch (error) {
-        throw error;
+        [
+          appointment.user_id,
+          appointment.activity_date,
+          appointment.activity_name,
+          appointment.activity_time,
+          appointment.activity_description,
+          appointment.activity_link,
+        ]
+      );
     }
+    console.log("created calendars!");
+  } catch (error) {
+    throw error;
+  }
 };
 
 const createInitialFavoriteRecipes = async () => {
-    try {
-        for (const favRecipe of favoriteRecipes) {
-            await client.query(
-                `
+  try {
+    for (const favRecipe of favoriteRecipes) {
+      await client.query(
+        `
             INSERT INTO favoriteRecipes(user_id, recipe_id)
             VALUES($1, $2);
         `,
-                [favRecipe.user_id, favRecipe.recipe_id]
-            );
-        }
-        console.log("created favorite recipes!");
-    } catch (error) {
-        throw error;
+        [favRecipe.user_id, favRecipe.recipe_id]
+      );
     }
+    console.log("created favorite recipes!");
+  } catch (error) {
+    throw error;
+  }
 };
 
 const createInitialFavoriteExercises = async () => {
-    try {
-        for (const favExercise of favoriteExercises) {
-            await client.query(
-                `
+  try {
+    for (const favExercise of favoriteExercises) {
+      await client.query(
+        `
             INSERT INTO favoriteExercises(user_id, exercise_id)
             VALUES($1, $2);
         `,
-                [favExercise.user_id, favExercise.exercise_id]
-            );
-        }
-
-        console.log("created favorite exercises!");
-    } catch (error) {
-        throw error;
+        [favExercise.user_id, favExercise.exercise_id]
+      );
     }
+
+    console.log("created favorite exercises!");
+  } catch (error) {
+    throw error;
+  }
 };
 
 const createInitialUsers = async () => {
-   
-    try {
-        for (const user of users) {
-           await client.query(`
+  try {
+    for (const user of users) {
+      await client.query(
+        `
                 INSERT INTO users(firstname, lastname, username, password, email)
                 VALUES($1, $2, $3, $4, $5);
-            `, [user.firstname, user.lastname, user.username, user.password, user.email]
-            )
-        }
-        console.log("created users")
-    } catch (error) {
-        throw error
+            `,
+        [
+          user.firstname,
+          user.lastname,
+          user.username,
+          user.password,
+          user.email,
+        ]
+      );
     }
+    console.log("created users");
+  } catch (error) {
+    throw error;
+  }
 };
 
 const createInitialExercises = async () => {
-    try {
-        for (const exercise of exercises) {
-            await client.query(`
+  try {
+    for (const exercise of exercises) {
+      await client.query(
+        `
                 INSERT INTO exercises(name, description, difficulty, imgUrl)
                 VALUES($1, $2, $3, $4);
-            `, [exercise.name, exercise.description, exercise.difficulty, exercise.imgUrl]
-            )
-        }
-        console.log("created exercises")
-    } catch (error) {
-        throw error
+            `,
+        [
+          exercise.name,
+          exercise.description,
+          exercise.difficulty,
+          exercise.imgUrl,
+        ]
+      );
     }
-}
+    console.log("created exercises");
+  } catch (error) {
+    throw error;
+  }
+};
 
 const createInitialSelfCare = async () => {
-    try {
-        for (const activity of selfCare) {
-            await client.query(
-                `
+  try {
+    for (const activity of selfCare) {
+      await client.query(
+        `
                 INSERT INTO selfCare(name, description, article_url)
                 VALUES($1, $2, $3)
                 `,
-                [activity.name, activity.description, activity.article_url]
-            )
-        }
-        console.log("created self care!")
-    } catch (error) {
-        throw error
+        [activity.name, activity.description, activity.article_url]
+      );
     }
-}
+    console.log("created self care!");
+  } catch (error) {
+    throw error;
+  }
+};
 
 const createInitialRecipes = async () => {
-    try {
-        for (const recipe of recipes) {
-            await client.query(
-                `
+  try {
+    for (const recipe of recipes) {
+      await client.query(
+        `
                 INSERT INTO recipes(name, difficulty, recipe_yield, imgUrl, description)
                 VALUES($1, $2, $3, $4, $5)
                 `,
-                [recipe.name, recipe.difficulty, recipe.recipe_yield, recipe.imgUrl, recipe.description]
-            )
-        }
-        console.log("created recipes!")
-    } catch (error) {
-        throw error
+        [
+          recipe.name,
+          recipe.difficulty,
+          recipe.recipe_yield,
+          recipe.imgUrl,
+          recipe.description,
+        ]
+      );
     }
-}
+    console.log("created recipes!");
+  } catch (error) {
+    throw error;
+  }
+};
 
 // Call all functions to build the db
 const buildDb = async () => {
-    try {
-        // ACTUALLY CONNECT TO THE LOCAL DB
-        client.connect()
+  try {
+    // ACTUALLY CONNECT TO THE LOCAL DB
+    client.connect();
 
-        // run the functions
-        await dropTables()
-        await createTables()
-        await createInitialUsers()
-        await createInitialExercises()
-        await createInitialSelfCare()
-        await createInitialRecipes()
-        await createInitialCalendars()
-        await createInitialFavoriteRecipes()
-        await createInitialFavoriteExercises()
-
-    } catch (error) {
-        console.error(error)
-    } finally {
-        // close the connection to the local db. will always run
-        client.end()
-    }
-}
-buildDb()
+    // run the functions
+    await dropTables();
+    await createTables();
+    await createInitialUsers();
+    await createInitialExercises();
+    await createInitialSelfCare();
+    await createInitialRecipes();
+    await createInitialCalendars();
+    await createInitialFavoriteRecipes();
+    await createInitialFavoriteExercises();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    // close the connection to the local db. will always run
+    client.end();
+  }
+};
+buildDb();
--- server/db/sqlHelperFunctions/selfCare.js
+++ server/db/sqlHelperFunctions/selfCare.js
@@ -1,82 +1,104 @@
-const client = require('../client');
+const client = require("../client");
 
 // GET - /api/selfCare - get all selfCare
 async function getAllSelfCare() {
-    try {
-        const { rows: selfCare } = await client.query(`SELECT * FROM selfCare`);
-        return selfCare;
-    } catch (error) {
-        throw new error(`Get all request did not work ${error.message} :(`)
-    }
+  try {
+    const { rows: selfCare } = await client.query(`SELECT * FROM selfCare`);
+    return selfCare;
+  } catch (error) {
+    throw new error(`Get all request did not work ${error.message} :(`);
+  }
 }
 
 // GET - /api/selfCare/:selfCare_id - get a single selfCare by id
 async function getSelfCareById(id) {
-    try {
-        const { rows: [selfCare] } = await client.query(`
+  try {
+    const {
+      rows: [selfCare],
+    } = await client.query(
+      `
             SELECT * FROM selfCare
             WHERE selfCare_id = $1;
-        `, [id]);
-        return selfCare;
-    } catch (error) {
-        throw new error(`Get all by request did not work ${error.message} :(`)
-    }
+        `,
+      [id]
+    );
+    return selfCare;
+  } catch (error) {
+    throw new error(`Get by ID request did not work ${error.message} :(`);
+  }
 }
 
 // POST - /api/selfCare - create a new selfCare
 async function createSelfCare(body) {
-    try {
-        const { rows: [selfCare] } = await client.query(`
+  try {
+    const {
+      rows: [selfCare],
+    } = await client.query(
+      `
         INSERT INTO selfCare(name, description, article_url)
         VALUES($1, $2, $3)
         RETURNING *;
-        `, [body.name, body.description, body.article_url]);
-        return selfCare;
-    } catch (error) {
-        throw new error(`Create new request did not work ${error.message} :(`)
-    }
+        `,
+      [body.name, body.description, body.article_url]
+    );
+    return selfCare;
+  } catch (error) {
+    throw new error(`Create new request did not work ${error.message} :(`);
+  }
 }
 // PUT - /api/selfCare/:selfCare_id - update a single selfCare by id
 async function updateSelfCare(id, fields = {}) {
-    const setString = Object.keys(fields).map((key, index) => `"${key}"=$${index + 1}`).join(', ');
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
 
-    // return early if this is called without fields
-    if (setString.length === 0) {
-        return;
-    }
+  // return early if this is called without fields
+  if (setString.length === 0) {
+    return;
+  }
 
-    try {
-        const { rows: [selfCare] } = await client.query(`
+  try {
+    const {
+      rows: [selfCare],
+    } = await client.query(
+      `
       UPDATE selfCare
       SET ${setString}
       WHERE selfCare_id=${id}
       RETURNING *;
-    `, Object.values(fields));
+    `,
+      Object.values(fields)
+    );
 
-        return selfCare;
-    } catch (error) {
-        throw new error(`Update by id did not work ${error.message} :(`)
-    }
+    return selfCare;
+  } catch (error) {
+    throw new error(`Update by id did not work ${error.message} :(`);
+  }
 }
 
 // DELETE - /api/selfCare/:selfCare_id - delete a single selfCare by id
 async function deleteSelfCareById(id) {
-    try {
-        const { rows: [selfCare] } = await client.query(`
+  try {
+    const {
+      rows: [selfCare],
+    } = await client.query(
+      `
       DELETE FROM selfCare
       WHERE selfCare_id=$1
       RETURNING *;
-    `, [id]);
-        return selfCare;
-    } catch (error) {
-        throw new error(`Delete request did not work ${error.message} :(`)
-    }
+    `,
+      [id]
+    );
+    return selfCare;
+  } catch (error) {
+    throw new error(`Delete request did not work ${error.message} :(`);
+  }
 }
 
 module.exports = {
-    getAllSelfCare,
-    getSelfCareById,
-    createSelfCare,
-    updateSelfCare,
-    deleteSelfCareById
-}
+  getAllSelfCare,
+  getSelfCareById,
+  createSelfCare,
+  updateSelfCare,
+  deleteSelfCareById,
+};
