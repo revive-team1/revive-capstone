25bf8369e83583b233d491a604e706e2fa01c38e
Cheyenne Pantoja <CheyennePantoja@gmail.com>
2024-02-06 12:53:26 -0800

    imported store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
-// import { store } from './app/store';
+import { store } from './components/SelfCare.jsx'
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
