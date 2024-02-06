9174f498ac762741e95d6e52bcc10c4827562a6b
sophshap <sophieashapiro@gmail.com>
2024-02-06 15:39:12 -0500

    routing done in App and links in Nav

--- client/src/components/Navigations.jsx
+++ client/src/components/Navigations.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Navigations = () => {
+
   return (
-    <div>Navigations</div>
+    <header id='navbar'>
+
+      <div id="navbarItems">
+
+        <Link to="/">home</Link>
+        <Link to="/excerises">fitness</Link>
+        <Link to="/selfcare">self care</Link>
+        <Link to="/recipes">food</Link>
+        <Link to="/login">login</Link>
+        <Link to="/register">register</Link>
+        <Link to="/account">my account</Link>
+        <Link to="/favorite-exercises">favorite exercises</Link>
+        <Link to="/favorite-recipes">favorite recipes</Link>
+
+      </div>
+    </header>
   )
 }
 
