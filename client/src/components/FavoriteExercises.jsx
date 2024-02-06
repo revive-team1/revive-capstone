3f7de855cb22010974240077e4b8b2c9151dbb0b
sophshap <sophieashapiro@gmail.com>
2024-02-06 12:45:26 -0500

    set up each component function

---
+++ client/src/components/FavoriteExercises.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FavoriteExercises = () => {
+  return (
+    <div>FavoriteExercises</div>
+  )
+}
+
+export default FavoriteExercises
---
+++ client/src/components/SingleSelfCare.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleSelfCare = () => {
+  return (
+    <div>SingleSelfCare</div>
+  )
+}
+
+export default SingleSelfCare
