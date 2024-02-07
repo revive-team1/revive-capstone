4c9b9522c44621b901ed961f42fc0de3954d279a
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2024-02-07 12:39:42 -0500

    making login changes, commited on server accidentally and not on client

--- client/src/components/Navigations.jsx
+++ client/src/components/Navigations.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { updateToken } from '../actions/actionsSlice'
+import { useDispatch, useSelector } from 'react-redux';
 
 const Navigations = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const token = useSelector((it) => it.actionsSlice.token)
+
+  const handleLogOut = () => {
+  dispatch(updateToken(''))
+  navigate('/login')
+}
 
   return (
     <header id='navbar'>
@@ -12,9 +22,20 @@ const Navigations = () => {
         <Link to="/exercises">fitness</Link>
         <Link to="/selfcare">self care</Link>
         <Link to="/recipes">food</Link>
-        <Link to="/login">login</Link>
-        <Link to="/register">register</Link>
-        <Link to="/account">my account</Link>
+        <div>
+        {(!token) ? (
+          <>
+          <Link to="/login">login</Link>
+          <Link to="/register">register</Link>
+          </>
+        ) : (
+          <>
+            <Link to="/account">my account</Link>
+            <button onClick={handleLogOut}>Logout</button>
+          </>
+        )}
+        </div>
+        
         <Link to="/favorite-exercises">favorite exercises</Link>
         <Link to="/favorite-recipes">favorite recipes</Link>
 
