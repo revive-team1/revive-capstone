c56773c6bf9768bbf7862d17c47e8d1fdb7aec04
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2024-02-12 15:45:17 -0500

    added nav bar styling and added workout seed stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,12 +21,29 @@ function App() {
 
   return (
     <>
+    <div>
+      <header className='header'>
       <Navigations />
+      </header>
       <h1>revive</h1>
-
+    </div>
       <Routes>
 
-        <Route path="/" />
+        <Route path="/" element = {
+          <section id='intro'>
+              <div className='container-lg'>
+                <div className='row justify-content-center align-items-center'>
+                  <div className='col-md-5 text-center text-md-start'>
+                  <h1 className='display-6'>Welcome to revive</h1>
+                    <p className='lead my-4 text-muted'>Your one stop shop to all that is wellness!</p>
+                  </div>
+                  <div className='col-md-6 text-center'>
+                    <p>hey, let's get healthy</p>
+                  </div>
+                </div>
+              </div>
+            </section>
+          }/>
         <Route path="/account" element={<Account user={user} favoriteExercises={favoriteExercises} setFavoriteExercises={setFavoriteExercises}/>} favoriteRecipes = {favoriteRecipes} setFavoriteRecipes={setFavoriteRecipes} />
         <Route path="/selfcare" element={<SelfCare />} />
         <Route path="/exercises" element={<Exercises user_id={user.user_id}/>} />
--- client/src/components/Navigations.jsx
+++ client/src/components/Navigations.jsx
@@ -9,35 +9,58 @@ const Navigations = () => {
   const token = useSelector((it) => it.actionsSlice.token)
 
   const handleLogOut = () => {
-  dispatch(updateToken(''))
-  navigate('/login')
-}
+    dispatch(updateToken(''))
+    navigate('/login')
+  }
 
   return (
-    <header id='navbar'>
-
-      <div id="navbarItems">
-
-        <Link to="/">home</Link>
-        <Link to="/exercises">fitness</Link>
-        <Link to="/selfcare">self care</Link>
-        <Link to="/recipes">food</Link>
-        <div>
-        {(!token) ? (
-          <>
-          <Link to="/login">login</Link>
-          <Link to="/register">register</Link>
-          </>
-        ) : (
-          <>
-            <Link to="/account">my account</Link>
-            <button onClick={handleLogOut}>Logout</button>
-          </>
-        )}
+    <div className='navbar navbar-expand-lg navbar-light fixed-top'>
+      <div className='container-xxl'>
+        <div className='navbar-brand'>
+          <Link to="/" className='fw-bold text-secondary'>revive</Link>
         </div>
-        
+        <div>
+          <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#main-nav' aria-controls='main-nav' aria-expanded='false' aria-label='Toggle Navigation'> <span className='navbar-toggler-icon'></span></button>
+          <div className='collapse navbar-collapse justify-content-end align-center' id='main-nav'>
+            <div className='navbar-nav'>
+              <div className='nav-item'>
+                <Link to="/exercises" className='nav-link'>fitness</Link>
+              </div>
+              <div className='nav-item'>
+                <Link to="/selfcare" className='nav-link'>self care</Link>
+              </div>
+              <div className='nav-item'>
+                <Link to="/recipes" className='nav-link'>food</Link>
+              </div>
+
+              {(!token) ? (
+                <>
+                  <div className='nav-item'>
+                    <Link to="/login" className='nav-link'>login</Link>
+                  </div>
+                  <div className='nav-item'>
+                    <Link to="/register" className='nav-link'>register</Link>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className='nav-item'>
+                    <Link to="/account" className='nav-link'>my account</Link>
+                  </div>
+                  <div className='nav-item d-md-none'>
+                    <button onClick={handleLogOut} className='btn btn-secondary'>Logout</button>
+                  </div>
+                  <div className='nav-item ms-2 d-none d-md-inline'>
+                    <button onClick={handleLogOut} className='btn btn-secondary'>Logout</button>
+                  </div>
+                </>
+              )}
+
+            </div>
+          </div>
+        </div>
       </div>
-    </header>
+    </div>
   )
 }
 
