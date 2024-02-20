d9de618905e3560ae11d530fc6a47aeb3eb64cbc
Melissa J. Chin <melissa1jchin@gmail.com>
2024-02-19 18:06:06 -0600

    added no events scheduled message; rendered event details on single day calendar page

--- client/src/components/SingleDay.jsx
+++ client/src/components/SingleDay.jsx
@@ -1,7 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import AddCalendarAppointment from './AddCalendarAppointment';
 
 function SingleDay({ user_id, date, setAppointments, appointments }) {
+  console.log(new Date(date))
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  let monthIndex = new Date(date).getMonth()
+  let month = months[monthIndex]
+  let day = (new Date(date).getDate() + 1)
+
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  let dayIndex = (new Date(date).getDay()+1)
+  let dayName = days[dayIndex]
+  let year = new Date(date).getFullYear()
+  let clickedDate = `${dayName} ${month} ${day}, ${year}`
+  const navigate= useNavigate()
+
   useEffect(() => {
 
     const fetchSingleCalendarDay = async () => {
@@ -20,14 +34,44 @@ function SingleDay({ user_id, date, setAppointments, appointments }) {
 
 
   return (
+
+  !appointments.length? (
+    <>
+    <h2> {clickedDate}</h2> <br/>
+      <p >No events scheduled.</p>
+      <div>
+        <button onClick={() => {
+          navigate(`/calendar`);
+        }}>See Calendar</button> 
+      </div>
+    </>) :
+    
    appointments.map((appointment)=> {
     return (
       <>
+      <br/>
+      <h2>{clickedDate}</h2> <br/>
       <h3>{appointment.activity_name}</h3>
       <h3> {appointment.activity_time}</h3>
+      <h3> {appointment.activity_description}</h3>
+      <h3> {appointment.activity_link}</h3> 
+      <br/>
+      <div className="breakLine"></div>
+      
+      <br/>
+      <div>
+        <button onClick={() => {
+          navigate(`/calendar`);
+        }}>See Calendar</button> 
+      </div>
       </>
+    )})
+    
+    
+    
     )
-   }))
   }
 
 export default SingleDay
+
+
