cfc1135474f7cf7e8b4908a6f0179f6269888b88
cturnbull19 <carolineturnbull19@email.com>
2024-02-28 15:03:39 -0500

    styling workouts

--- client/src/components/Workouts.jsx
+++ client/src/components/Workouts.jsx
@@ -4,7 +4,7 @@ import { useGetWorkoutsQuery } from '../api/fetching'
 import { useSelector } from 'react-redux'
 import { useState } from 'react'
 import FavoriteWorkoutExercisesButton from './FavoriteWorkoutExercisesButton'
-import newGymBackground from '../assets/newGymBackground.jpg'
+import WorkoutPhoto from '../assets/WorkoutPhoto.jpg'
 
 const Workouts = ({ user_id }) => {
     const token = useSelector((it) => it.actionsSlice.token)
@@ -23,38 +23,38 @@ const Workouts = ({ user_id }) => {
 
     return (
         <>
-            <div className='card bg-none text-white p-0 m-1'>
-                <img src={newGymBackground} alt='interior of a gym' className='card-img img-responsive' width={'auto'} height={'375px'}></img>
-                <div className='card-img-overlay d-flex justify-content-center'>
-                    <div className='row'>
-                        <div >
-                            <h1 className='card-title m-4'>Trainer Spotlight</h1>
-                            <button className='btn btn-light m-1'>
-                                <a href='https://www.tiktok.com/@mdjfitness/video/7335900513386482950?_r=1&_t=8kDAWO4zGdL' target='blank' className='nav-link'>tiktok</a>
-                            </button>
-                            <button className='btn btn-light'>
-                                <a href='https://www.tiktok.com/@nataleebfitness/video/7321693098667511072?_r=1&_t=8kDAMIdFsT8' className='nav-link' target='blank'>tiktok</a>
-                            </button>
-                            <button className='btn btn-light m-1'>
-                                <a href='https://www.tiktok.com/@tamaraaanthonyy/video/7335225800150584619?_r=1&_t=8kD94NW1ZSp' className='nav-link' target='blank'>tiktok</a>
-                            </button>
-                        </div>
-                    </div>
+            <div id='spotlight' className='border-none p-5 m-5'>
+                <div className='d-flex justify-content-center'>
+                    <h1 className='m-4'>Trainer Spotlight</h1>
                 </div>
-            </div>
-
-            <div>
-                <div>
-                    <h1>Workout Library</h1>
+                <div className='longBreakLine'></div>
+                <br></br>
+                <div className='d-flex flex-wrap justify-content-center'>
+                    <button className='btn btn-light m-1'>
+                        <a href='https://www.tiktok.com/
+                        @mdjfitness/video/7335900513386482950?_r=1&_t=8kDAWO4zGdL' target='blank' className='nav-link'>tiktok</a>
+                    </button>
+                    <button className='btn btn-light'>
+                        <a href='https://www.tiktok.com/@nataleebfitness/video/7321693098667511072?_r=1&_t=8kDAMIdFsT8' className='nav-link' target='blank'>tiktok</a>
+                    </button>
+                    <button className='btn btn-light m-1'>
+                        <a href='https://www.tiktok.com/@tamaraaanthonyy/video/7335225800150584619?_r=1&_t=8kD94NW1ZSp' className='nav-link' target='blank'>tiktok</a>
+                    </button>
+                    <br />
                 </div>
+                <br />
+                <img className='accountPhoto' src={WorkoutPhoto}></img>
+                <br />
+                <br />
+                <h2 className='fw-light'>Workout Library</h2>
+                <br />
                 <form className='d-flex justify-content-center'>
                     <input
-                        className='form-control w-25 text-center' placeholder="Search workouts here..."
+                        className='form-control w-25 text-center' placeholder="Search workouts..."
                         onChange={(e) => setSearch(e.target.value)}
                     />
                 </form>
             </div>
-            <br></br>
             <div>
                 <>
                     <div className='row'>
